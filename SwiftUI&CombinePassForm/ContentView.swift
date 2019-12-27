4de271e3b4939aa7b3a62e6d2c1074b8c7f6ecd1
FelixCoderArt <falkovskyfelix@gmail.com>
2019-12-27 20:08:42 +0300

    Creat Form

--- SwiftUI&CombinePassForm/ContentView.swift
+++ SwiftUI&CombinePassForm/ContentView.swift
@@ -9,13 +9,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var firstName = ""
+    @State var lastName = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                VStack {
+                    VStack {
+                        Group {
+                            TextField("First Name", text: $firstName)
+                                .padding(12)
+                        }
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .shadow(radius: 5)
+                        
+                        Group {
+                            TextField("First Name", text: $lastName)
+                                .padding(12)
+                        }
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .shadow(radius: 5)
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Group {
+                                Text("Create")
+                                    .foregroundColor(Color.white)
+                                    .padding(12)
+                                }
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                            .shadow(radius: 2)
+                        }
+                    }.padding(12)
+                }.background(Color.gray)
+                
+                List {
+                    Text("Placeholder")
+                }
+            }.navigationBarTitle(Text("Restration Form"))
+        }
     }
 }
-
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone XR")
     }
 }
+#endif
