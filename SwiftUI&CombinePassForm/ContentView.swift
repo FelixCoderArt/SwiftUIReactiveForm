c539ddb40acad4c7076b5c3c829c8da40ae8e992
FelixCoderArt <falkovskyfelix@gmail.com>
2019-12-27 17:16:00 +0300

    Initial Commit

---
+++ SwiftUI&CombinePassForm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI&CombinePassForm
+//
+//  Created by Felix Falkovsky on 27.12.2019.
+//  Copyright © 2019 Felix Falkovsky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
